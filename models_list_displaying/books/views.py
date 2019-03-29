c8ca11780eaf1a0bcfcc510cfba669c79ac0649a
ZibyMax <max@rekstar-nn.ru>
2019-03-29 20:47:14 +0300

    добавил третье задание

---
+++ models_list_displaying/books/views.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from django.views import generic
+
+from books.models import Book
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+#    def get_context_data(self, **kwargs):
+#        pass
+
+    def get_queryset(self, **kwargs):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        print(f'{year}+{month}+{day}')
+        if year and month and day:
+            pub_date = datetime.strptime(f'{year}.{month}.{day}', '%Y.%m.%d')
+            print(pub_date)
+            return super(BookListView, self).get_queryset().filter(pub_date=pub_date)
+        return super(BookListView, self).get_queryset()
