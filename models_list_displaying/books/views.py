d9eb5bf32b2aaf27567a9122eb5abb2cd9b5f657
ZibyMax <max@rekstar-nn.ru>
2019-03-30 10:55:29 +0300

    stop

--- models_list_displaying/books/views.py
+++ models_list_displaying/books/views.py
@@ -16,10 +16,13 @@ class BookListDateView(generic.ListView):
     def get_context_data(self, **kwargs):
         str_date = self.kwargs.get('date')
         pub_date = datetime.strptime(f'{str_date}', '%Y-%m-%d')
-        context = super(BookListDateView, self).get_context_data(**kwargs).filter('pub_date')
-        #context['book_list'] = context['book_list'].filter('pub_date')
+        context = super(BookListDateView, self).get_context_data(**kwargs)
+        print(context['page_obj'])
         return context
 
     def get_queryset(self, **kwargs):
         context = super(BookListDateView, self).get_queryset().order_by('pub_date')
         return context
+
+        #context = super(BookListDateView, self).get_context_data(**kwargs).filter('pub_date')
+        #context['book_list'] = context['book_list'].filter('pub_date')
